c5ff2670f3c75336b59e51eb7ad88660d0089bd9
UtkarshSingh597 <utkarshhh1511@gmail.com>
2026-07-10 08:22:17 +0530

    JWT Authentication implemented

---
+++ src/main/java/com/utkarsh/file_nest/security/JwtService.java
@@ -0,0 +1,34 @@
+package com.utkarsh.file_nest.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                   .subject(email)
+                   .issuedAt(new Date())
+                   .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                   .signWith(signingKey())
+                   .compact();
+    }
+
+    private SecretKey signingKey() {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
